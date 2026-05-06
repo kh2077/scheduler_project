75b957832a625dce2aaed2a9a81575b36e80026e
Mohamed Emad <modyhero18@gmail.com>
2026-05-06 23:13:05 +0300

    Create GanttEntry class with constructor

---
+++ src/Models/GnattEntry.java
@@ -0,0 +1,11 @@
+public class GanttEntry {
+    public String pid;
+    public int start;
+    public int end;
+
+    public GanttEntry(String pid, int start, int end) {
+        this.pid   = pid;
+        this.start = start;
+        this.end   = end;
+    }
+}
